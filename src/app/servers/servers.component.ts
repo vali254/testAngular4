3fc0479fae44a9b125203c383d9a6179699380e6
vali254 <valentinvlasceanu@gmail.com>
2017-10-25 17:15:45 +0300

    end stage 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowAddNewButton = false;
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
+  usernameValue: string ='';
+  canResetUsername = false;
 
-  constructor() { }
+  displayDetailsValue: string = 'This is the text to be toggled for the Display Button!';
+  toggledValueForDisplayButton = false;
+  logData: any[] = [];
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+
+  onUpdateUsername(event: Event){
+    //  tmpValue = (<HTMLInputElement>event.target).value;
+      if((<HTMLInputElement>event.target).value == '') {
+          this.canResetUsername = false;
+        }else{
+          this.canResetUsername = true;
+        }
+  }
+
+  onResetUsername(){
+    if(  this.canResetUsername == true){
+      this.usernameValue = '';
+      this.canResetUsername = false;
+    }
+  }
+
+  getColor(rowIndex){
+    return rowIndex > 5 ? 'blue' : 'white';
+  }
+
+  changeToogledValue(){
+    this.toggledValueForDisplayButton = !this.toggledValueForDisplayButton;
+    var noElements = this.logData.length;
+    console.log('noElements - ',noElements);
+    noElements++;
+    this.logData.push(noElements);
+
   }
 
 }
